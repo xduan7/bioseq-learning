fd5f8093d20b487d23d3de3e2439ff3197a8b285
Xiaotian Duan <xduan7@gmail.com>
2020-10-04 14:29:47 -0500

    add unit test function for iterable dataset

--- tests/test_genome_dataset.py
+++ tests/test_genome_dataset.py
@@ -6,11 +6,12 @@ File Description:
 
 """
 import os
-import random
 import unittest
-from typing import List, Union
+from typing import List
 
-from src.datasets import GenomeDataset
+from torch.utils.data import DataLoader
+
+from src.datasets import GenomeDataset, GenomeIterDataset
 from src.datasets.genome_dataset import \
     PADDING_CHAR, NUCLEOTIDE_CHAR_INDEX_DICT
 
@@ -26,28 +27,46 @@ _TEST_GENOME_DIR_PATHS: List[str] = [
 _TEST_SEQ_LEN: int = 100
 _TEST_NUM_MASKS: int = 5
 _TEST_MAX_NUM_PADDINGS: int = 10
+_TEST_BATCH_SIZE: int = 16
 _PADDING_INDEX: int = NUCLEOTIDE_CHAR_INDEX_DICT[PADDING_CHAR]
 
 
 class TestGenomeDataset(unittest.TestCase):
-    """unittest class for 'masked_genome_dataset' class
+    """unittest class for 'genome_dataset' and 'genome_iter_dataset' classes
     """
     def test_genome_dataset(self):
-        """test 'masked_genome_dataset' class
+        """test 'genome_dataset' class
         """
-        masked_genome_dataset = GenomeDataset(
+        genome_dataset = GenomeDataset(
             genome_dir_paths=_TEST_GENOME_DIR_PATHS,
             seq_len=_TEST_SEQ_LEN,
             max_num_paddings=_TEST_MAX_NUM_PADDINGS,
         )
 
-        for _i in range(len(masked_genome_dataset)):
-            _indexed_seq, _padding_mask = masked_genome_dataset[_i]
+        for _i in range(len(genome_dataset)):
+            _indexed_seq, _padding_mask = genome_dataset[_i]
             # test if all the indexed values are in the index dict
             assert set([_v.item() for _v in _indexed_seq.unique()]).issubset(
                 set(NUCLEOTIDE_CHAR_INDEX_DICT.values()))
             # test if the number of paddings is valid (no bigger)
             assert _padding_mask.sum().item() <= _TEST_MAX_NUM_PADDINGS
+
+    def test_genome_iter_dataset(self):
+        """test 'genome_iter_dataset' class
+        """
+        genome_iter_dataloader = DataLoader(
+            GenomeIterDataset(
+                genome_dir_paths=_TEST_GENOME_DIR_PATHS,
+                seq_len=_TEST_SEQ_LEN,
+                max_num_paddings=_TEST_MAX_NUM_PADDINGS,
+            ),
+            batch_size=_TEST_BATCH_SIZE,
+        )
+
+        for _batch_data in genome_iter_dataloader:
+            assert _batch_data[0].shape == (_TEST_BATCH_SIZE, _TEST_SEQ_LEN)
+            assert _batch_data[1].shape == (_TEST_BATCH_SIZE, _TEST_SEQ_LEN)
+            break
 
 
 if __name__ == '__main__':
