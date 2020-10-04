33510cdb538816dbb7288795d6ef9f0b9f9a5a87
Xiaotian Duan <xduan7@gmail.com>
2020-10-04 14:43:02 -0500

    fill in comments for sequence mask

--- src/datasets/sequence_mask.py
+++ src/datasets/sequence_mask.py
@@ -11,22 +11,21 @@ from typing import Tuple, Iterable, Optional
 import torch
 
 
-class SequenceMask():
-
+class SequenceMask:
+    """mask generator class for genome sequences
+    """
     def __init__(
             self,
             seq_len: int,
             num_masks: int,
             init_masked_indices: Optional[Iterable[int]] = None,
     ):
-        """
-        TODO: docstring
+        """initialize a mask generator
+
         :param seq_len:
         :type seq_len:
         :param init_masked_indices:
         :type init_masked_indices:
-        :return:
-        :rtype:
         """
         assert 0 < num_masks < seq_len
 
@@ -34,6 +33,10 @@ class SequenceMask():
         self._num_masks: int = num_masks
 
         # generate the default masks for both attention and source sequences
+        # attention masks is a float 2D tensor of shape seq_len * seq_len,
+        # where -inf represents the attention 'black hole'
+        # sequence mask is a boolean 1D tensor of shape seq_len, where True
+        # represents masks
         self._attn_mask: torch.FloatTensor
         self._src_mask: torch.BoolTensor
         self._attn_mask, self._src_mask = self.update(init_masked_indices)
@@ -42,12 +45,14 @@ class SequenceMask():
             self,
             masked_indices: Optional[Iterable[int]] = None,
     ) -> Tuple[torch.FloatTensor, torch.BoolTensor]:
-        """
-        TODO: docstring
-        :param masked_indices:
-        :type masked_indices:
-        :return:
-        :rtype:
+        """update the attention and sequence masks
+
+        :param masked_indices: optional indices for masking, randomly
+        generate a a list of mask indices if it's not given
+        :type masked_indices: iterable of integer or None
+        :return: attention masks (float 2D tensor of shape seq_len * seq_len)
+        and the sequence mask (boolean 1D tensor of shape seq_len)
+        :rtype: a tuple of float tensor and a boolean tensor
         """
 
         _masked_indices = masked_indices if masked_indices else \
@@ -68,18 +73,18 @@ class SequenceMask():
 
         return self._attn_mask, self._src_mask
 
-    def get_attn_mask(self):
-        """
-        TODO: docstring
-        :return:
-        :rtype:
+    def get_attn_mask(self) -> torch.FloatTensor:
+        """get the attention mask
+
+        :return: attention masks (float 2D tensor of shape seq_len * seq_len)
+        :rtype: float tensor
         """
         return self._attn_mask
 
-    def get_src_mask(self):
-        """
-        TODO: docstring
-        :return:
-        :rtype:
+    def get_src_mask(self) -> torch.BoolTensor:
+        """get the sequence mask
+
+        :return: sequence mask (boolean 1D tensor of shape seq_len)
+        :rtype: boolean tensor
         """
         return self._src_mask
