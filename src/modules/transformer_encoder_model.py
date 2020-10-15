1dd6f56b455c2c24d83a2ff9ebe1949bf3ca9a89
Xiaotian Duan <xduan7@gmail.com>
2020-10-15 14:55:12 -0500

    convert transformer encoder to nn.sequential for easier parallelization

--- src/modules/transformer_encoder_model.py
+++ src/modules/transformer_encoder_model.py
@@ -6,6 +6,7 @@ File Description:
 
 """
 from typing import Optional
+from collections import OrderedDict
 
 import torch
 import torch.nn as nn
@@ -37,43 +38,46 @@ class TransformerEncoderModel(nn.Module):
     ):
         super(TransformerEncoderModel, self).__init__()
 
-        self._seq_len = seq_len
+        self._seq_len: int = seq_len
+        self._pos_enc: bool = pos_enc
+        self._xfmr_enc_layer_norm: bool = xfmr_enc_layer_norm
+        self._xfmr_enc_num_layers: int = xfmr_enc_num_layers
 
         # TODO: add k-mer embedding option
-        self._emb = nn.Embedding(
+        _layers = OrderedDict()
+        _layers['emb'] = nn.Embedding(
             num_embeddings=num_tokens,
             embedding_dim=emb_dim,
             padding_idx=padding_index,
         )
-        self._pos_enc = PositionalEncoding(
-            seq_len=seq_len,
-            emb_dim=emb_dim,
-            dropout=pos_enc_dropout,
-            emb_scale=pos_enc_emb_scale,
-        ) if pos_enc else None
-        _xfmr_enc_layer = nn.TransformerEncoderLayer(
-            d_model=emb_dim,
-            nhead=xfmr_enc_layer_num_attn_heads,
-            dim_feedforward=xfmr_enc_layer_feedforward_dim,
-            activation=xfmr_enc_layer_activation,
-            dropout=xfmr_enc_layer_dropout,
-        )
-        _xfmr_enc_layer_norm = nn.LayerNorm(
-            normalized_shape=[seq_len, batch_size, emb_dim]
-        ) if xfmr_enc_layer_norm else None
-        self._xfmr_enc = nn.TransformerEncoder(
-            encoder_layer=_xfmr_enc_layer,
-            num_layers=xfmr_enc_num_layers,
-            norm=_xfmr_enc_layer_norm,
-        )
-        self._dec = nn.Linear(emb_dim, num_tokens)
+        if pos_enc:
+            _layers['pos_enc'] = PositionalEncoding(
+                seq_len=seq_len,
+                emb_dim=emb_dim,
+                dropout=pos_enc_dropout,
+                emb_scale=pos_enc_emb_scale,
+            )
+        for _i in range(1, xfmr_enc_num_layers + 1):
+            _layers[f'xfmr_enc_layer_{_i}'] = nn.TransformerEncoderLayer(
+                d_model=emb_dim,
+                nhead=xfmr_enc_layer_num_attn_heads,
+                dim_feedforward=xfmr_enc_layer_feedforward_dim,
+                activation=xfmr_enc_layer_activation,
+                dropout=xfmr_enc_layer_dropout,
+            )
+        if xfmr_enc_layer_norm:
+            _layers['xfmr_enc_layer_norm'] = nn.LayerNorm(
+                normalized_shape=[seq_len, batch_size, emb_dim]
+            )
+        _layers['dec'] = nn.Linear(emb_dim, num_tokens)
+        self._layers = nn.Sequential(_layers)
 
-        self._init_weights()
+        # self._init_weights()
 
     def _init_weights(self):
         # the initialization methods and parameters are purely random
-        nn.init.normal_(self._emb.weight, mean=0.0, std=1.0)
-        nn.init.normal_(self._dec.weight, mean=0.0, std=1.0)
+        nn.init.normal_(getattr(self._layers, 'emb').weight, mean=0.0, std=1.0)
+        nn.init.normal_(getattr(self._layers, 'dec').weight, mean=0.0, std=1.0)
 
     def forward(
             self,
@@ -82,23 +86,13 @@ class TransformerEncoderModel(nn.Module):
             padding_mask: Optional[torch.BoolTensor],
     ) -> torch.FloatTensor:
 
-        # dynamically generate a mask of size (seq_len, seq_len)
-        # indicates the accessibility of the tokens for the ith sample
-        # _masked_indices = random.sample(range(self._seq_len), num_masks)
-        # _mask = torch.zeros(size=(self._seq_len, ), dtype=torch.float)
-        # _mask.scatter_(0, torch.LongTensor(_masked_indices), float('-inf'))
-        # _mask = _mask.repeat(self._seq_len).view(-1, self._seq_len)
-        # _mask = _mask.to(src.device)
-        #
-        # _tmp = copy.deepcopy(src)
-        # _tmp[_mask[0].bool()] = 0
-
-        _tmp = self._emb(src)
+        _tmp = getattr(self._layers, 'emb')(src)
         if self._pos_enc:
-            _tmp = self._pos_enc(_tmp)
-        _tmp = self._xfmr_enc(
-            src=_tmp,
-            mask=attn_mask,
-            src_key_padding_mask=padding_mask,
-        )
-        return self._dec(_tmp)
+            _tmp = getattr(self._layers, 'pos_enc')(_tmp)
+        for _i in range(1, self._xfmr_enc_num_layers + 1):
+            _tmp = getattr(self._layers, f'xfmr_enc_layer_{_i}')(
+                src=_tmp,
+                src_mask=attn_mask,
+                src_key_padding_mask=padding_mask,
+            )
+        return getattr(self._layers, 'dec')(_tmp)
