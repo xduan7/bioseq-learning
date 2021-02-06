de840ab3afd3180b19c87fb8a02941f65dfac0a1
Xiaotian Duan <xduan7@gmail.com>
2021-02-06 16:10:16 -0600

    add ref & rep bacteria genome processing script

---
+++ scripts/process_patric_ref_n_rep_bacteria_faa_genomes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# change working directory to the directory of this script
+PROJECT_DIR="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )")"
+
+# process genomes in parallel
+export PYTHONPATH=${PROJECT_DIR}:$PYTHONPATH
+echo ${PROJECT_DIR}
+nohup python ${PROJECT_DIR}/src/datasets/process_patric_faa_genomes.py \
+  -i ${PROJECT_DIR}/data/raw/genomes/reference_or_representative_bacteria \
+  -o ${PROJECT_DIR}/data/interim/genomes/reference_or_representative_bacteria \
+  -w 240 \
+  >> ${PROJECT_DIR}/logs/process_patric_ref_or_rep_bacteria_faa_genomes.txt &
