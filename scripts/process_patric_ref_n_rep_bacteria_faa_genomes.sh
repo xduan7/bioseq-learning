b962a0b9cc3b05bd59a95c962d3f008f22d456fa
Xiaotian Duan <xduan7@gmail.com>
2021-02-07 11:42:28 -0600

    remove the nohup from process_patric_ref_n_rep_bacteria_faa_genomes.sh

--- scripts/process_patric_ref_n_rep_bacteria_faa_genomes.sh
+++ scripts/process_patric_ref_n_rep_bacteria_faa_genomes.sh
@@ -5,9 +5,8 @@ PROJECT_DIR="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1
 
 # process genomes in parallel
 export PYTHONPATH=${PROJECT_DIR}:$PYTHONPATH
-echo ${PROJECT_DIR}
-nohup python ${PROJECT_DIR}/src/datasets/process_patric_faa_genomes.py \
+python ${PROJECT_DIR}/src/datasets/process_patric_faa_genomes.py \
   -i ${PROJECT_DIR}/data/raw/genomes/reference_or_representative_bacteria \
   -o ${PROJECT_DIR}/data/interim/genomes/reference_or_representative_bacteria \
-  -w 240 \
-  >> ${PROJECT_DIR}/logs/process_patric_ref_or_rep_bacteria_faa_genomes.txt &
+  -w 160 \
+  2> ${PROJECT_DIR}/logs/process_patric_ref_or_rep_bacteria_faa_genomes.txt
