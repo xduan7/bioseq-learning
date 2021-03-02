8cd8dfc9cc9c7c011ad39d11887a7ed9544cfe90
Xiaotian Duan <xduan7@gmail.com>
2021-03-02 17:03:33 -0600

    add conserved domain histogram and sunburst

--- scripts/visualize_reference_or_representative_bacteria.py
+++ scripts/visualize_reference_or_representative_bacteria.py
@@ -7,8 +7,7 @@ File Description:
 Visualization of Reference or Representative Bacteria
 - [x] organism distribution (pie chart)
 - [x] organism distribution (sunburst)
-- [ ] number of contigs per genome distribution (histogram)
-- [ ] number of conserved domains per coding region
+- [x] number of conserved domains per coding region
 - [ ] conserved domain frequency (maybe a pie chart)
 
 """
@@ -33,7 +32,7 @@ from src.datasets.genome_domain_dataset import (
 )
 
 
-ANNOTATION = Annotation.PATRIC
+ANNOTATION = Annotation.RefSeq
 COLORS = plt.cm.Pastel1.colors
 
 REF_OR_REP_BACTERIA_CONTIGS_WITH_CDS_FILE_PATH = \
@@ -237,3 +236,135 @@ if not os.path.exists(_fig2_path):
     )
     fig2.write_html(_fig2_path)
     fig2.write_image(_fig2_path.replace('html', 'png'), scale=1.0)
+
+
+# visualization of the number of conserved domains per CDS histogram
+_fig3_path = \
+    f'../docs/images/reference_or_representative_bacteria/' \
+    f'conserved_domains_hist.{ANNOTATION.value}.html'
+if not os.path.exists(_fig3_path):
+    num_conserved_domains_per_cds = pd.Series([])
+    for __contig_with_cds in tqdm(contigs_with_cds):
+        __contig_with_cds: ContigWithConservedDomains
+        __contig_feature_df = __contig_with_cds.contig_feature_df[
+            __contig_with_cds.contig_feature_df['feature_type'] == 'CDS'
+        ]
+
+        __contig_cds_ids = __contig_feature_df['patric_id'].values \
+            if ANNOTATION == Annotation.PATRIC else \
+            __contig_feature_df['refseq_locus_tag'].values
+
+        # only specific hits
+        __contig_conserved_domain_df = \
+            __contig_with_cds.contig_conserved_domain_df[
+                __contig_with_cds.contig_conserved_domain_df['hit_type']
+                == 'Specific'
+            ]
+
+        def __get_annotation_id(__seq_id: str):
+            if __seq_id.count('|') == 1:
+                return __seq_id
+            elif __seq_id.count('|') >= 2 \
+                    and ANNOTATION == Annotation.PATRIC:
+                return __seq_id.rstrip('|').rsplit('|', 1)[0]
+            elif __seq_id.count('|') >= 2 \
+                    and ANNOTATION == Annotation.RefSeq:
+                return __seq_id.rstrip('|').rsplit('|', 2)[1]
+            else:
+                _warning_msg = \
+                    f'cannot parse the PATRIC ID from FASTA ' \
+                    f'sequence record with name {__seq_id}.'
+                print(_warning_msg)
+                return ''
+
+        __contig_conserved_domain_df.loc[:, 'seq_id'] = \
+            __contig_conserved_domain_df['seq_id'].apply(
+                __get_annotation_id)
+
+        __num_conserved_domains_per_cds = \
+            __contig_conserved_domain_df['seq_id'].value_counts()
+
+        for __id in __contig_cds_ids:
+            if __id not in __num_conserved_domains_per_cds:
+                __num_conserved_domains_per_cds[__id] = 0
+
+        num_conserved_domains_per_cds = \
+            num_conserved_domains_per_cds.append(__num_conserved_domains_per_cds)
+
+    _label = 'number of conserved domains per CDS'
+    _fig3 = px.histogram(
+        num_conserved_domains_per_cds.to_frame(_label),
+        x=_label,
+        nbins=int(num_conserved_domains_per_cds.max() + 1),
+        title=f'Histogram for Number of Conserved Domains Per Coding '
+              f'Region with {ANNOTATION.value} Annotations',
+        width=4000,
+        height=1000,
+    )
+    _fig3.update_layout(title_x=0.5)
+    _fig3.write_html(_fig3_path)
+    _fig3.write_image(_fig3_path.replace('html', 'png'), scale=1.0)
+
+
+
+# visualization of the conserved domain sunburst
+_fig4_path = \
+    f'../docs/images/reference_or_representative_bacteria/' \
+    f'conserved_domains_sunburst.{ANNOTATION.value}.html'
+if not os.path.exists(_fig4_path):
+    __cols = ['accession', 'superfamily_accession']
+    conserved_domain_df = pd.DataFrame([], columns=__cols)
+    for __contig_with_cds in tqdm(contigs_with_cds):
+        __contig_with_cds: ContigWithConservedDomains
+        __conserved_domain_df = \
+            __contig_with_cds.contig_conserved_domain_df[
+                __contig_with_cds.contig_conserved_domain_df[
+                    'hit_type'] == 'Specific'][__cols]
+        conserved_domain_df = pd.concat(
+            [conserved_domain_df, __conserved_domain_df])
+
+    conserved_domain_df = conserved_domain_df.fillna('')
+
+    accession_thresholds = [0.0005, 0.0005]
+    for __acc, __thresh in zip(__cols, accession_thresholds):
+        __counts = conserved_domain_df[__acc].value_counts()
+
+        if isinstance(__thresh, float):
+            __remove_rank = __counts[
+                __counts < len(conserved_domain_df) * __thresh].index.to_list()
+        else:
+            __remove_rank = __counts[
+                __counts < __thresh].index.to_list()
+
+        conserved_domain_df.loc[
+            conserved_domain_df[__acc].isin(__remove_rank), __acc] = \
+            f'other accessions'
+
+    _fig4 = px.sunburst(
+        data_frame=conserved_domain_df,
+        path=['superfamily_accession', 'accession'],
+    )
+    ids = _fig4.data[0]['ids']
+    labels = _fig4.data[0]['labels']
+    values = _fig4.data[0]['values']
+    parents = _fig4.data[0]['parents']
+    _indices = np.array([
+        'other' not in __l or __p == ''
+        for __l, __p in zip(labels,  parents)
+    ])
+    _fig4.update_traces(
+        ids=ids[_indices],
+        labels=labels[_indices],
+        values=values[_indices],
+        parents=parents[_indices],
+        insidetextorientation='radial',
+    )
+    _fig4.update_layout(
+        height=4000,
+        width=4000,
+        uniformtext=dict(minsize=10, mode='hide'),
+        title=f'Conserved Domains Distribution ({ANNOTATION.value}) ',
+        title_x=0.5,
+    )
+    _fig4.write_html(_fig4_path)
+    _fig4.write_image(_fig4_path.replace('html', 'png'), scale=1.0)
